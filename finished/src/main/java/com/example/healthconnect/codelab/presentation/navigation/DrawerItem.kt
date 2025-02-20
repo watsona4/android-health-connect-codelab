222efd86ae186c9b30a602cc6fc8b4253ec4bc22
Aaron M. Watson <watsona4@gmail.com>
2025-02-20 10:07:20 -0500

    removed exercise screens

--- finished/src/main/java/com/example/healthconnect/codelab/presentation/navigation/DrawerItem.kt
+++ finished/src/main/java/com/example/healthconnect/codelab/presentation/navigation/DrawerItem.kt
@@ -64,7 +64,7 @@ fun DrawerItem(
 fun DrawerItemPreview() {
   HealthConnectTheme {
     DrawerItem(
-      item = Screen.ExerciseSessions,
+      item = Screen.InputReadings,
       selected = true,
       onItemClick = {}
     )
--- finished/src/main/java/com/example/healthconnect/codelab/presentation/navigation/Screen.kt
+++ finished/src/main/java/com/example/healthconnect/codelab/presentation/navigation/Screen.kt
@@ -29,8 +29,6 @@ const val UID_NAV_ARGUMENT = "uid"
  */
 enum class Screen(val route: String, val titleId: Int, val hasMenuItem: Boolean = true) {
   WelcomeScreen("welcome_screen", R.string.welcome_screen, false),
-  ExerciseSessions("exercise_sessions", R.string.exercise_sessions),
-  ExerciseSessionDetail("exercise_session_detail", R.string.exercise_session_detail, false),
   InputReadings("input_readings", R.string.input_readings),
   DifferentialChanges("differential_changes", R.string.differential_changes),
   PrivacyPolicy("privacy_policy", R.string.privacy_policy, false)
